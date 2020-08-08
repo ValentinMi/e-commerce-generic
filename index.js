44017eae5c2cfbb3313ecbbd350078ca8ae560cb
Valentin Misiaszek <mowerv6@gmail.com>
2020-08-08 22:23:58 +0200

    feat(server): add static dir for img serving

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 require("./startup/db")();
 require("./startup/cors")(app);
